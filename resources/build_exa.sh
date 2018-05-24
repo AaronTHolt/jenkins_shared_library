e9f8f40dc33a46ba046a4dd97ca977b0f4c35e30
AaronTHolt <holtat@colorado.edu>
2018-05-24 11:56:25 -0600

    Split build into clone and build

--- resources/build_exa.sh
+++ resources/build_exa.sh
@@ -1,5 +1,1 @@
-git clone https://gitlab-ci-token:${MFIX_EXA_TOKEN}@mfix.netl.doe.gov/gitlab/exa/mfix.git
-cd mfix
-git checkout develop
-cd ..
 sudo /usr/local/bin/singularity build mfix_full.img mfix_full.def
