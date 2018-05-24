cfc9385484c4d30870d5160121e7376885e1f81f
AaronTHolt <holtat@colorado.edu>
2018-05-24 11:53:54 -0600

    Add build command to build script

--- resources/build_exa.sh
+++ resources/build_exa.sh
@@ -2,3 +2,4 @@ git clone https://gitlab-ci-token:${MFIX_EXA_TOKEN}@mfix.netl.doe.gov/gitlab/exa
 cd mfix
 git checkout develop
 cd ..
+sudo /usr/local/bin/singularity build mfix_full.img mfix_full.def
