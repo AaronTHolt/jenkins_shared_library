8ba08637884ac246bf718d96e595bc0d0aa3a516
AaronTHolt <holtat@colorado.edu>
2018-05-24 15:09:10 -0600

    Fix comments

--- vars/mfixPipeline.groovy
+++ vars/mfixPipeline.groovy
@@ -20,19 +20,19 @@ def call(def job) {
         // inside this block your credentials will be available as env variables
         wrap([$class: 'VaultBuildWrapper', configuration: configuration, vaultSecrets: secrets]) {
             try {
-                # Clone exa repository, checkout develop branch
+                // Clone exa repository, checkout develop branch
                 stage('Clone') {
                     sh(libraryResource('mfix/clone_exa.sh'))
                 }
-                # Build singularity image
+                // Build singularity image
                 stage('Build'){
                     sh(libraryResource('mfix/build_exa.sh'))
                 }
-                # Push to sregistry
+                // Push to sregistry
                 stage('Push') {
                     sh(libraryResource('mfix/push_exa.sh'))
                 }
-                # Setup sregistry-cli on Summit if needed, submit Slurm job
+                // Setup sregistry-cli on Summit if needed, submit Slurm job
                 stage('Summit') {
                     sshagent(credentials: ['holtat-scompile']) {
                         sh 'ssh -o StrictHostKeyChecking=no -l holtat scompile.rc.int.colorado.edu "echo $SREG_ESCAPED > /home/holtat/sreg_tmp"'
